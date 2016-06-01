c4602436d1f60b759b0688bfad3747c29928734f
Alex.k <Alex.k@ramotion.com>
2016-06-01 14:59:25 +0300

    shared scheme

---
+++ PaperSwitchDemo/PaperSwitch/PaperSwitch.h
@@ -0,0 +1,19 @@
+//
+//  PaperSwitch.h
+//  PaperSwitch
+//
+//  Created by Alex K. on 01/06/16.
+//  Copyright © 2016 Ramotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PaperSwitch.
+FOUNDATION_EXPORT double PaperSwitchVersionNumber;
+
+//! Project version string for PaperSwitch.
+FOUNDATION_EXPORT const unsigned char PaperSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PaperSwitch/PublicHeader.h>
+
+
